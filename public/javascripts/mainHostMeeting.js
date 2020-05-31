d15612508c8d166d5742542f776a50275355cb17
shashank prakash <38760033+shashank404error@users.noreply.github.com>
2020-05-31 22:27:10 +0530

    Update mainHostMeeting.js

--- public/javascripts/mainHostMeeting.js
+++ public/javascripts/mainHostMeeting.js
@@ -139,7 +139,7 @@ var localVideo = document.querySelector('#localVideo');
 var remoteVideo = document.querySelector('#remoteVideo');
 
 navigator.mediaDevices.getUserMedia({
-    audio: false,
+    audio: true,
     video: true
 })
     .then(gotStream)
@@ -158,7 +158,8 @@ function gotStream(stream) {
 }
 
 var constraints = {
-    video: true
+    video: true,
+    audio: true
 };
 
 console.log('Getting user media with constraints', constraints);
